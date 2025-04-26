190b9f106abf3bcbcc8f6dd27cf06081f7845229
Tomasz Waszczyk <tomasz@waszczyk.com>
2025-04-26 17:41:05 +0200

    moved files

--- feedzen/src/App.tsx
+++ feedzen/src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import NostrLogin from './components/NostrLogin'
-import NostrFeed from './components/NostrFeed'
+import NostrLogin from './components/feed/NostrLogin'
+import NostrFeed from './components/feed/NostrFeed'
 
 function App() {
   const [count, setCount] = useState(0)
